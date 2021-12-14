4488500fa6509a7e5c143495e8ca3d2f15955528
Gabriel Mendes de Campos <87998234+GabrielMendesdc@users.noreply.github.com>
2021-12-14 13:38:16 -0300

    now it shows a thumbnail and the length of file

--- yt.py
+++ yt.py
@@ -1,96 +1,107 @@
 from pytube import YouTube
 from tkinter import *  
+import urllib.request as url
+from PIL import Image, ImageTk
+from urllib.request import urlopen
+import io
+
+def pegaentry(link):
+    global img
+    link = entry.get()
+    ind = link.index('=') + 1
+    try:
+        letra = link.index('&')
+        imgUrl = f'https://i.ytimg.com/vi/{link[ind:letra]}/hqdefault.jpg'
+    except:
+         imgUrl = f'https://i.ytimg.com/vi/{link[ind:]}/hqdefault.jpg'
+            
+    url.urlretrieve(imgUrl, "C:/Users/pdv/desktop/thumb.png")
+    img = ImageTk.PhotoImage(file = "C:/Users/pdv/desktop/thumb.png")
+    frame = Label(janela)
+    bt = Canvas(frame)
+    bt.create_image(0, 0, image=img, anchor=NW)
+    bt.grid(row=contador())
+    frame.grid()
+    return faz(link)
+
 
 link, yt = '', ''
 i=2
 
-def pegaentry(link):
-    link = entry.get()
-    return faz(link)
-
-
-def contador():
-    global i
-    i += 1
-    return i-1
-    
-    
 root = Tk()
 root.title('YouTube Downloader')
 
 janela = Label(root, bg='pink')
 janela.grid()
 
-entry = Entry(janela, width = 30,textvariable = link, bg='white', font=("Arial", 20))
+entry = Entry(janela, width = 25,textvariable = link, bg='white', font=("Arial", 18))
 entry.bind('<Return>',pegaentry)
 entry.grid(row=1)
 
-texto = Text(janela, height = 1, bg='white', width = 30, font=("Arial", 20))
+texto = Text(janela, height = 1, bg='white', width = 25, font=("Arial", 18))
 texto.tag_configure("tag_name", justify='center')
 texto.insert("1.0", "Digite sua URL: ")
 texto.tag_add("tag_name", "1.0", "end")
 texto.grid(row=0)
 
-def criabtn(x,stream):
-    botao = Button(janela,text=stream,bg='white',font=("Arial", 20), command = lambda: baixar(x))
-    botao.grid(row=contador())
 
+def contador():
+    global i
+    i += 1
+    return i-1
+
+    
+def criabtn(x,stream):
+    botao = Button(janela,text=stream,bg='white',font=("Arial", 18), command = lambda: baixar(x))
+    botao.grid(row=contador())
+    
     
 def baixar(x):
     ys = yt.streams.get_by_itag(x)
-    print(x)
     ys.download('C:/Users/pdv/desktop/')
+    init()
 
     
-def sep(i, types):
+def sep(i, types, length):
     i = str(i)
     lista = i.split('"')
     if types == 'audio':
         importante = lista[1:6:2]
+        importante.append(str(length))
         print(importante)
+        importante[1] = importante[1].replace('/mp4', '')
         criabtn(importante[0],importante[1:])
     else:
         importante = lista[1:8:2]
+        importante.append(length)
         print(importante)
+        importante[1] = importante[1].replace('/mp4', '')
         criabtn(importante[0],importante[1:])
         
+    
+def gigas(length):
+    print('tamanho', length)
+    listo = ['','bytes', 'KB', 'MB', 'GB', 'TB', 'YB']
+    for i in range(8):
+        if length <= 1024 ** i:
+            tam = length / (1024 ** (i-1))
+            tam = round(tam)
+            stri = str(f'{tam}{listo[i]}')
+            return stri
+            
 
-def tempo(length):
-    if length >= 60:
-        if length % 60 != 0:
-            return f"Tamanho do vídeo: {length // 60} minutos e {length % 60} segundos"
-        else:
-            return f"Tamanho do vídeo: {length // 60} minutos"
-    else:
-        return f"Tamanho do vídeo: {length} segundos"  
-    
-    
 def faz(link):
     global yt
     yt = YouTube(link)
-    #Título do Vídeo
-    print("Título do vídeo: ",yt.title)
-    #Número de views
-    print("Números de views: ",yt.views)
-    #Tamanho do Vídeo
-    print(tempo(yt.length))
-    #Nota
-    print("Porcentagem Likes: ",f"{(yt.rating*20):.2f}", "%")
-    #All Streams
-    print()
-    print("Video streams avaliable: \n")
+    j=0
     for i in yt.streams.filter(only_video=True, mime_type="video/mp4"):
-        sep(i, 'video')
-        
-    texto = Text(janela, height = 1, bg='white', width = 30, font=("Arial", 20))
-    texto.tag_configure("tag_name", justify='center')
-    texto.insert("1.0", "Apenas audio")
-    texto.tag_add("tag_name", "1.0", "end")
-    texto.grid(row=contador())
-    
-    print()
-    print("Audio streams avaliable: \n")
+        tam = gigas(i.filesize_approx)
+        if not j >= 5:
+            sep(i, 'video', tam)
+        j+=1    
     for i in yt.streams.filter(only_audio=True, mime_type="audio/mp4"):
-        sep(i, 'audio')
-    
+        tam = gigas(i.filesize_approx)
+        sep(i, 'audio', tam)
+
+        
 root.mainloop()
